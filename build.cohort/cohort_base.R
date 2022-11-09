aa64d8fb8530d93d6b52dba9330608b5fbe9a2e4
Eric Tornabell Noguera <etornabell@idiapjgol.info>
2022-11-09 16:01:55 +0100

    primera versio final del cohort

--- build.cohort/cohort_base.R
+++ build.cohort/cohort_base.R
@@ -17,7 +17,8 @@ setDT(cohort.base)
 
 # Comorbiditats
 for(f_nm in f_nms){
-# CI, diabetes, estenosi mitral, FA, HTA, ictus, MRC, obesitat morbida, obesitat, protesi valvular, sobrepes, TEP, trombofilitis, valvulopatia arotica
+# CI, diabetes, estenosi mitral, FA, HTA, ictus, MRC, obesitat morbida, obesitat, protesi valvular, sobrepes,
+# TEP, trombofilitis, valvulopatia arotica
   print(f_nm)
   load(paste0(paste0('~/idiap/projects/INR/build.data/','/', f_nm)))
   setDT(inr)
@@ -45,9 +46,10 @@ cohort.base = full_join(cohort.base, vacunes)
 nrow(cohort.base[is.na(VA_U_DOSI), .N, keyby=.(id)]) 
 
 # Temps en rang terapeutic
-load('~/idiap/projects/INR/build.data/any.RData')
-load('~/idiap/projects/INR/build.data/migany.RData')
-
+# load('~/idiap/projects/INR/build.data/any.RData')
+# load('~/idiap/projects/INR/build.data/migany.RData')
+# trt = cohort.base %>% select(id, EDAT, USUA_SEXE, DATA_NAIX, data_vacuna) %>% left_join(any)
+# trt = trt %>% mutate(dies_vac_trt = dat_trt_12M-data_vacuna)
 # cohort.base = full_join(cohort.base, any)
 # cohort.base = full_join(cohort.base, migany)
 
@@ -61,12 +63,21 @@ cohort.base = full_join(cohort.base, tto)
 
 # INR
 load('~/idiap/projects/INR/build.data/var_inr.RData')
-# load('~/idiap/projects/INR/build.data/var_inr2.RData')
+inr = cohort.base %>% select(id, EDAT, USUA_SEXE, DATA_NAIX, data_vacuna) %>% left_join(inr)
+inr = inr %>% mutate(dies_vac_inr = difftime(dat_inr20, data_vacuna, units = 'days'))
+inr = inr[inr$dies_vac_inr>=0]
+inr = inr %>% group_by(id) %>% summarize_if(is.double, min)
+cohort.base = full_join(cohort.base, inr)
 
 
 # Exclusions
 cohort.base = cohort.base[!(is.na(cohort.base$VA_U_COD))] # Eliminem els no vacunats
 cohort.base = cohort.base[!(is.na(cod_tto))] # Ens quedem només amb els que tenen tractament amb warfarina o sintrom
 cohort.base = cohort.base[EDAT>18] # Treiem els menors de 18 anys
+cohort.base = cohort.base[as.numeric(cohort.base$dies_vac_inr)<=42,] # Fem fora els que tenen més de 42 des de la vacuna fins al seguent control INR
+cohort.base = cohort.base[!(is.na(dies_vac_inr))] # També fem fora la gent que no presenta cap control de INR
 
-# write_xlsx(cohort.base,"~/idiap/projects/INR/build.data/cohort.xlsx")
+# Reordenem la base de dades
+cohort.base = cohort.base[, c(1,3,4,5,19,20,21,22,23,24,25,26,27,28,2,6,7,8,9,10,11,12,13,14,15,16,17,18)]
+# Guardem
+write_xlsx(cohort.base,"~/idiap/projects/INR/build.cohort/cohort.xlsx")
